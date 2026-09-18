cb78af79c6cdcc99ecd074e6e6f970e49e124036
Ivan Vysocinas <firstpicktinker@gmail.com>
2026-09-18 18:05:40 +0300

    Configure GitHub Pages deployment and update SEO URLs

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export default function App() {
 
   return (
     <I18nProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio">
         <SmoothScroll />
         <ScrollToTop />
         <PageTransitionProvider>
